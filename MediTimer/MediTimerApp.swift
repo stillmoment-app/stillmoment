576126fb20c391a26b2eab53749c0622225036f9
Helmut Zechmann <793372+HelmutZechmann@users.noreply.github.com>
2025-10-26 12:23:51 +0100

    Initial Commit

---
+++ MediTimer/MediTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediTimerApp.swift
+//  MediTimer
+//
+//  Created by Helmut Zechmann on 26.10.25.
+//
+
+import SwiftUI
+
+@main
+struct MediTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediTimerTests/MediTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediTimerTests.swift
+//  MediTimerTests
+//
+//  Created by Helmut Zechmann on 26.10.25.
+//
+
+import Testing
+@testable import MediTimer
+
+struct MediTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
