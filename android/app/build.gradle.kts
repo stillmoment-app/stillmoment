11ca295945c669a48d36a3cc1845512cd5f715f3
Helmut Zechmann <793372+HelmutZechmann@users.noreply.github.com>
2026-01-17 16:43:37 +0100

    chore(android): Prepare release v1.9.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.stillmoment"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.8.0"
+        versionCode = 12
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "com.stillmoment.HiltTestRunner"
     }
